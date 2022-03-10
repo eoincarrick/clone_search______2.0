69ec77c1ef9a8402cd08097a3231bbb01b4d74bf
Eoin Carrick <22carri99@gmail.com>
2022-03-10 23:56:57 +0000

    c

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import {}
+import { Routes, Redirect } from "react-router-dom";
 
 const Route = () => {
-  return <div className="p-4">Route</div>;
+  return (
+    <div className="p-4">
+      <Routes>
+        <Route exact path="/">
+          <Redirect to="/search" />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default Route;
