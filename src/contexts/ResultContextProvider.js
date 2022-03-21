1642c129c224436fc6de07effbf466ae920d3c34
Eoin Carrick <22carri99@gmail.com>
2022-03-21 23:55:41 +0000

    code

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -7,16 +7,37 @@ export const resultContextProvider = ({ children }) => {
   const [result, setResult] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
-
+  console.log(setResult);
   const getResult = async (type) => {
-    setIsLoading(true);
+    setIsLoading(!isLoading);
 
-    const response = await fetch(`${baseUrl}${type}`, {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-host': 'google-search1.p.rapidapi.com',
-        'x-rapidapi-key': 'f01687c65emsheaaef840bfca25dp1de079jsn9678bec10359',
-      },
-    });
-  }
+    try {
+      const response = await fetch(`${baseUrl}${type}`, {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'google-search1.p.rapidapi.com',
+          'x-rapidapi-key':
+            'f01687c65emsheaaef840bfca25dp1de079jsn9678bec10359',
+        },
+      });
+
+      const data = await response.json();
+      setIsLoading(isLoading);
+
+      setResult(data);
+    } catch (error) {
+      console.log(`Error Message`);
+      console.log(error);
+    }
+  };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResult, result, searchTerm, setSearchTerm, isLoading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
 };
+
+export const useResultContext = () => useContext(ResultContext);
