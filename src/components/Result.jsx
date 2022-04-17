aed4544230145d3db9e3e35379abbbfdf8683161
Eoin Carrick <22carri99@gmail.com>
2022-04-17 22:10:47 +0000

    result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -12,9 +12,15 @@ const Result = () => {
   console.log(result);
   const location = useLocation();
 
-  // useEffect(() => {
-  //   getResult('/search/q=Rock&num=40');
-  // }, []);
+  useEffect(() => {
+    if (searchTerm) {
+      if (location.pathname === '/videos') {
+        getResult(`/search/q=${searchTerm} videos`);
+      } else {
+        getResult(`${location.pathname}/q=${searchTerm}&num=40`);
+      }
+    }
+  }, [searchTerm, location.pathname]);
 
   if (isLoading) return <Loading />;
 
@@ -37,9 +43,37 @@ const Result = () => {
         </div>
       );
     case '/images':
-      return 'SEARCH';
+      return (
+        <div className='flex flex-wrap justify-center items-center'>
+          {result?.image_result?.map(
+            ({ image, link: { href, title } }, index) => {
+              return (
+                <a href={href} key={index} className='sm:p-3 p-5'>
+                  <img src={image?.src} alt={title} loading='lazy' />
+                  <p className='w-36 break-words text-sm mt-2'>{title}</p>
+                </a>
+              );
+            }
+          )}
+        </div>
+      );
     case '/news':
-      return 'SEARCH';
+      return (
+        <div className='flex flex-wrap justify-between space-y-6 sm:px-56 items-center'>
+          {result?.entries?.map(({ links, id, title, source }) => (
+            <div key={id} title={description} className='md:w-2/5 w-full '>
+              <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+                {title}
+              </p>
+              <a href={links?.[0].href} target='_blank' title={link} rel='noreferrer' className='hover'>
+                <p className='text-sm'>
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
     case '/videos':
       return 'SEARCH';
 
